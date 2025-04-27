e86b68a9ba1432f51bf81ce8cb586c946e9be8a4
Naga <nagamohan.koganti@gmail.com>
2025-04-27 12:54:32 +0530

    Model Trainer changes

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -99,7 +99,7 @@ class ModelTrainer:
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+            logging.info(f"Best found model : '{best_model_name}' on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -111,9 +111,6 @@ class ModelTrainer:
             r2_square = r2_score(y_test, predicted)
             return r2_square
             
-
-
-
             
         except Exception as e:
             raise CustomException(e,sys)
